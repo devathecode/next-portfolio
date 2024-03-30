db9b6e288b3e75c8ae12208522e3763607c0b4e5
devathecode <devanshuv1501@gmail.com>
2024-03-31 01:58:24 +0530

    Updated description meta data for home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,7 @@ import { poppins } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Devanshu verma | Frontend developer",
-  description: `I hail from Gorakhpur, a city in the northern part of India, and I see myself as a creative craftsman. I love building things, whether they are digital products or effective teams.
-  When it comes to developing user interfaces. I enjoy using the latest technologies such as (Angular, React, Vue etc..) and other modern tools to create visually stunning and highly functional web applications for both businesses and consumers alike.
-  With my passion for design and a keen attention to detail, I believe I can help bring your vision to life`,
+  description: `India's creative craftsman building great web apps. I leverage Angular, React, Vue.js for UIs for businesses & consumers. Keen eye for detail realizes your vision.`,
   keywords: ['Angular', 'Angular developers', 'Nextjs', 'Nextjs developers', 'React', 'React developers', 'Javascript developers', 'Web Development'],
   icons: {
     icon: "/devanshu.png"
