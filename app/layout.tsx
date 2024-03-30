6b583f16ae1817f4d124929d0e9595713b26b520
devathecode <devanshuv1501@gmail.com>
2024-03-31 01:45:24 +0530

    Added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,10 @@ export const metadata: Metadata = {
   description: `I hail from Gorakhpur, a city in the northern part of India, and I see myself as a creative craftsman. I love building things, whether they are digital products or effective teams.
   When it comes to developing user interfaces. I enjoy using the latest technologies such as (Angular, React, Vue etc..) and other modern tools to create visually stunning and highly functional web applications for both businesses and consumers alike.
   With my passion for design and a keen attention to detail, I believe I can help bring your vision to life`,
-  keywords: ['Angular', 'Angular developers', 'Nextjs', 'Nextjs developers', 'React', 'React developers', 'Javascript developers', 'Web Development']
+  keywords: ['Angular', 'Angular developers', 'Nextjs', 'Nextjs developers', 'React', 'React developers', 'Javascript developers', 'Web Development'],
+  icons: {
+    icon: "/devanshu.png"
+  }
 };
 
 export default function RootLayout({
