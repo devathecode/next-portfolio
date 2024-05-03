d95191aa6aff23ef5a532be5e38f5580f9b62527
devathecode <devanshuv1501@gmail.com>
2024-05-04 00:40:23 +0530

    Contact us section made working"

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -3,11 +3,29 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function contactSubmit(formData: FormData): Promise<string | null> {
     try {
-        console.log('formData', formData);
-        return null
+        const response = await fetch('https://dev-cms-backend.vercel.app/api/v1/crud/', {
+            headers: {
+                'origin': 'http://localhost:3000',
+                'content-type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({
+                name: formData.get('name'),
+                email: formData.get('email'),
+                message: formData.get('message'),
+                date: new Date().toLocaleDateString(),
+                time: new Date().toLocaleTimeString('en-US', {
+                    hour12: false,
+                    hour: "numeric",
+                    minute: "numeric"
+                })
+            })
+        });
+        const data = await response.json();
+        return '/thankyou'
     }
     catch (error) {
-        console.log('error', error);
-        return '/login'
+        // console.log('error', error);
+        return null
     }
 }
---
+++ app/thankyou/page.tsx
@@ -0,0 +1,14 @@
+const Thankyou = () => {
+    return (
+        <div className="cpm">
+            <div className="grid grid-cols-12 gap-2 px-5 py-2 my-32 text-center">
+                <div className="col-span-12 text-white my-auto order-2 md:order-1">
+                    <h1 className="text-4xl font-bold text-center text-yellow-700">Thank you for reaching out!</h1>
+                    <p className="text-center mt-4">We will contact you <span className="text-yellow-600">soon</span>.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Thankyou;
